26bc728a4c442fc98c57ed89a68bd28a6a0cc0d4
thibautunsinger <thibaut.unsinger@gmail.com>
2024-02-06 10:37:54 +0100

    changement workdir pour plus propre

--- Back-end/start.sh
+++ Back-end/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-psql -U kogbebou;
-npx prisma migrate dev;
-npm start;
+if [ -d /backend ]
+then
+  cd /backend || exit
+  psql -U kogbebou;
+  npx prisma migrate dev;
+  npm start;
+else
+  echo "backend directory doesnt exist"
+fi
