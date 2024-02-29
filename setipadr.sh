60de9feb66b71ecdfe6914e6f6722598d7bdf6b3
thibautunsinger <thibaut.unsinger@gmail.com>
2024-02-29 18:09:59 +0100

    script pour changer l'ip

---
+++ setipadr.sh
@@ -0,0 +1,3 @@
+sed -i '' -e 's/"http:\/\/localhost"/$("hostname -i")/g' './Back-end/.env'
+sed -i '' -e 's/"http:\/\/localhost"/$("hostname -i")/g' './Back-end/.env'
+sed -i '' -e 's/"http:\/\/localhost"/$("hostname -i")/g' './Front-end/src/config.ts'
